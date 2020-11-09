56046bee05c7dd8115ff5ee34464b5e18d46e256
cooc-hi-tech <data5885@gmail.com>
2020-11-09 09:13:01 +0800

    Initial commit

---
+++ try-mf/netconf4.py
@@ -0,0 +1,28 @@
+from ncclient import manager
+from lxml import etree
+
+cisco_device01 ={"host":"192.168.1.94",
+                     "port":"830",
+                     "username":"admin",
+                     "password":"cisco123",
+                     "hostkey_verify":False,
+                     "device_params":{"name":"iosxe"}}
+
+#system-type iosxe iosxr nxos
+device = manager.connect(**cisco_device01)
+
+
+get_interface1 = """
+<filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+    <interfaces xmlns="urn:ietf:params:xml:ns:yang:ietf-interfaces">
+        <interface>
+            <name>GigabitEthernet1</name>
+        </interface>
+     </interfaces>
+</filter>
+"""
+
+config=device.get(get_interface1)
+print(etree.tostring(config.data_ele,pretty_print=True))
+print(type(get_interface1))
+
